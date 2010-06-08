144535641eb0210767bc44ba5b125a23386545ce
felixonta@gmail.com <felixonta@gmail.com>
2010-06-08 22:13:49 +0200

    DBus service file to launch wiican-service on demand Installation setup file updated with the new changes Udev rule moved to priority 99 as solution described in LP:312957

--- setup.py
+++ setup.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys, os, platform
+import glob
+import subprocess
+
 from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.install_data import install_data
+from distutils.log import warn, info, error, fatal
+from distutils.dep_util import newer
+
+# Get current Python version
+python_version = platform.python_version_tuple()
+
+# Setup the default install prefix
+prefix = sys.prefix
+
+# Check our python is version 2.6 or higher
+if python_version[0] >= 2 and python_version[1] >= 6:
+    ## Set file location prefix accordingly
+    prefix = '/usr/local'
+
+# Get the install prefix if one is specified from the command line
+for arg in sys.argv:
+    if arg.startswith('--prefix='):
+        prefix = arg[9:]
+        prefix = os.path.expandvars(prefix)
+
+# Gen .in files with @PREFIX@ replaced
+for filename in ['org.gnome.wiican.service']:
+    infile = open(filename + '.in', 'r')
+    data = infile.read().replace('@PREFIX@', prefix)
+    infile.close()
+
+    outfile = open(filename, 'w')
+    outfile.write(data)
+    outfile.close()
 
 setup(
         name='wiican',
-        version='0.2.1',
+        version='0.3',
         description='Wiimote manager',
         long_description='Wiican its a systray that wrappers wminput',
         author='J. Félix Ontañón',
@@ -13,7 +48,7 @@ setup(
         url='http://launchpad.net/wiican',
 
         classifiers=[
-            'Development Status :: 0.2.1 - Alpha',
+            'Development Status :: 0.3 - Alpha',
             'Environment :: Desktop Environment',
             'Intended Audience :: End Users/Desktop',
             'License :: OSI Approved :: GNU General Public License (GPL)',
@@ -25,7 +60,7 @@ setup(
         requires = ['PyGTK', 'dbuspython', 'PyYAML'],
 
         packages = ['wiican'],
-        scripts = ['bin/wiican'],
+        scripts = ['bin/wiican', 'bin/wiican-service'],
         data_files = [('share/wiican/config_skel', 
                         ['config_skel/mouse.wminput', 
                         'config_skel/neverball.wminput']),
@@ -33,6 +68,8 @@ setup(
                           ['img/wiitrayoff.svg', 'img/wiitrayon.svg',
                           'img/wiitrayon1.svg', 'img/wiitrayon2.svg', 
                           'img/wiitrayon3.svg']),
+                      ('share/dbus-1/services',
+                          ['org.gnome.wiican.service']),
                       ('share/wiican', 
                           ['about.ui','mapping.ui','entry.ui', 'wiican.svg']),
                       ('share/applications', ['wiican.desktop']),
