aa24d3f07138e8ced11ca74dd72cf524d464bf1e
J. Félix Ontañón <felixonta@gmail.com>
2010-06-20 00:46:53 +0200

    New Mapping class definition Added install-export to mapping manager

--- wiican/mappingmanager.py
+++ wiican/mappingmanager.py
@@ -19,109 +19,137 @@
 # Authors : J. Félix Ontañón <felixonta@gmail.com>
 # 
 ###
+
 import os
+import shutil
 import tarfile
 import tempfile
 import exceptions
 
 from xdg.DesktopEntry import DesktopEntry
 from xdg.IconTheme import getIconPath
-from xdg.BaseDirectory import xdg_cache_home
 
 from defs import ICON_DEFAULT
 
 class Mapping(object):
-    def __init__(self, name='', comment='', version='', authors='', 
-            icon_path='', mapping_path=''):
-        
-        self.name = name
-        self.comment = comment
-        self.version = version
-        self.authors = authors
-        self.icon_path = icon_path
-        self.mapping_path = mapping_path
-
-class MappingPackage(object):
-    pattern = '*.tgz'
-
-    @staticmethod
-    def load(package_path):
-        package_filename = os.path.splitext(os.path.basename(package_path))[0]
-        extract_path = os.path.join(xdg_cache_home, 'wiican', package_filename)
-
-        package_file = tarfile.open(package_path)
-        
-        if not 'info.desktop' in package_file.getnames():
-            raise MappingPackageError, 'Not info.desktop file found'
-    
-        if not 'mapping.wminput' in package_file.getnames():
-            raise MappingPackageError, 'Not mapping.wminput file found'
-
-        package_file.extractall(extract_path)
-
+    def __init__(self, info_path, mapping_path):
         # Get freedesktop definition file
-        info = DesktopEntry()
-        info.parse(os.path.join(extract_path, 'info.desktop'))
-        
+        self.__info = DesktopEntry()
+        self.__info.parse(info_path)
+
+        mapping_fp = open(mapping_path, 'r')
+        self.mapping = mapping_fp.read()
+        mapping_fp.close()
+
         # Get icon file path
-        icon_name = info.getIcon()
-        if icon_name in package_file.getnames(): # Icon included
-            icon_path = info.parse(os.path.join(extract_path, icon_name))
+        icon_name = self.__info.getIcon()
+        if icon_name in os.listdir(os.path.dirname(info_path)): # Icon included
+            self.icon_path = os.path.join(os.path.dirname(info_path), icon_name)
         elif getIconPath(icon_name): # Theme icon
-            icon_path = getIconPath(icon_name)
+            self.icon_path = getIconPath(icon_name)
         else:
-            icon_path = ICON_DEFAULT # Default icon
-
-        # Get wminput mapping definition file path
-        mapping_file = os.path.join(extract_path, 'mapping.wminput')
+            self.icon_path = ICON_DEFAULT # Default icon
+            
+    def get_name(self):
+        return self.__info.getName()
         
-        package_file.close()
+    def set_name(self, name):
+        self.__info.set('Name', name)
+        self.__info.set('Name', name, locale=True)
 
-        return Mapping(info.getName(), info.getComment(), info.getVersion(), 
-            info.get('X-Authors'), icon_path, mapping_file)
+    def get_comment(self):
+        return self.__info.getComment()
+
+    def set_comment(self, comment):
+        self.__info.set('Comment', comment)
+        self.__info.set('Comment', comment, locale=True)
+
+    def get_authors(self):
+        self.__info.get('X-Authors')
+
+    def set_authors(self, authors):
+        self.__info.set('X-Authors', authors)
+
+    def get_version(self):
+        self.__info.getVersion()
+
+    def set_version(self, version):
+        self.__info.setVersion(version)
+
+    def write(self, dest_path):
+        self.__info.write(os.path.join(dest_path, 'info.desktop'))
+        mapping_fp = open(os.path.join(dest_path, 'mapping.wminput'), 'w')
+        mapping_fp.write(self.mapping)
+        mapping_fp.close()
+
+    def __repr__(self):
+        return "Mapping <" + self.__info.get('Name', locale=False) + ' ' + \
+            str(self.__info.getVersion()) + ">"
         
-    @staticmethod
-    def save(mapping, mapping_path):
-        pass
-
-class MappingPackageError(exceptions.Exception):
-    pass
-
-
-import glob
-
 class MappingManager(object):
-    def __init__(self, user_paths=[], system_paths=[]):
+    def __init__(self, home_path, system_paths=[]):
 
-        self.user_paths = user_paths
+        self.home_path = home_path
         self.system_paths = system_paths
 
-        if not type(self.user_paths) is list:
-            self.user_paths = [self.user_paths]
-            
         if not type(self.system_paths) is list:
             self.system_paths = [self.system_paths]
         
-        self.mappings = {}
+        self.mapping_bag = {}
             
-    def scan_mappings(self):
-        new_mappings = {}
-        for path in self.system_paths + self.user_paths:
-            for mapping_path in glob.glob(os.path.join(path, MappingPackage.pattern)):
-                package_filename = os.path.splitext(os.path.basename(package_path))[0]
-                new_mappings.update({package_filename: MappingPackage.load(mapping_path)})
-                
-        new_mappings.update(self.mappings)
-        self.mappings = new_mappings
+    def scan(self):
+        def load_mapping(mappings, dirname, fnames):
+            if 'info.desktop' in fnames and 'mapping.wminput' in fnames:
+                mapping_name = os.path.splitext(os.path.basename(dirname))[0]
+                mappings[mapping_name] = {'path': dirname, 
+                    'mapping': Mapping(os.path.join(dirname, 
+                    'info.desktop'), os.path.join(dirname, 'mapping.wminput'))}
 
-    def install(self, mapping_name):
-        pass
+        for path in self.system_paths + [self.home_path]:
+            os.path.walk(path, load_mapping, self.mapping_bag)
+                
+    def install(self, package_path):
+        package_file = tarfile.open(package_path)
+        
+        if not 'info.desktop' in package_file.getnames():
+            raise MappingManagerError, 'Not info.desktop file found'
+    
+        if not 'mapping.wminput' in package_file.getnames():
+            raise MappingManagerError, 'Not mapping.wminput file found'
+
+        mapping_name = os.path.splitext(os.path.basename(package_path))[0]
+        mapping_path = os.path.join(self.home_path, mapping_name)
+        
+        package_file.extractall(mapping_path)
+        package_file.close()
+
+        mapping = Mapping(os.path.join(mapping_path, 'info.desktop'), 
+            os.path.join(mapping_path, 'mapping.wminput'))
+ 
+        self.mapping_bag[mapping_name] = {'path': mapping_path, 'mapping':
+            mapping}
+
+    def export(self, mapping_name, dest_path):
+        if not mapping_name in self.mapping_bag:
+            raise MappingManagerError, 'Mapping not found:' + ' ' + mapping_name
+
+        mapping = self.mapping_bag[mapping_name]['mapping']
+        mapping_path = self.mapping_bag[mapping_name]['path']
+        package_file = tarfile.TarFile(os.path.join(dest_path, 
+            mapping_name+'.tgz'), 'w')
+
+        for f in os.listdir(mapping_path):
+            package_file.add(os.path.join(mapping_path, f), arcname=f)
+            
+        package_file.close()
         
     def remove(self, mapping_name):
-        pass
-        
-    def get_mapping_list(self):
-        pass
-        
-    def get_mapping(self):
-        pass
+        if not mapping_name in self.mapping_bag:
+            raise MappingManagerError, 'Mapping not found:' + ' ' + mapping_name
+    
+        mapping_path = self.mapping_bag[mapping_name]['path']
+        shutil.rmtree(mapping_path)
+        self.mapping_bag.pop(mapping_name)
+
+class MappingManagerError(exceptions.Exception):
+    pass
